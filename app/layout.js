a3a7bbb2119e6c5528d8e60ab22ab0c36a5644a5
Daniel <danielturcanu15@gmail.com>
2026-08-27 11:25:47 +0300

    formate

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,8 @@ import "./globals.css";
 
 export const metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL || "https://angel-detailing-nu.vercel.app/",
+    process.env.NEXT_PUBLIC_SITE_URL ||
+      "https://angel-detailing-nu.vercel.app/",
   ),
   title: {
     default: "ANGEL DETAILING | Учет",
